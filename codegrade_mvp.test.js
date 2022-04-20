7734b5b0daa78de714cd370c66c191cf0702fb8f
Ryan Berry <rberry16@hotmail.com>
2022-04-19 23:23:52 -0700

    all tests passing

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -6,7 +6,7 @@ import { render, fireEvent, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 jest.setTimeout(1000) // default 5000 too long for Codegrade
-const waitForOptions = { timeout: 100 }
+const waitForOptions = { timeout: 500 }
 const queryOptions = { exact: false }
 
 let up, down, left, right, reset, submit
